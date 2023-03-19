27ac1a1600a444cc3816d2eedf3b59145608c644
Coopykat <125597494+Coopykat@users.noreply.github.com>
2023-03-19 12:04:12 +0100

    add files via upload
    first files added from

---
+++ athenry.js
@@ -0,0 +1,12 @@
+  let athenry = "The fields of Athenry";
+
+const slam = document.querySelector("h1");
+slam.addEventListener('mouseover', function() {
+document.body.innerText = "The fields of Athenry";
+slam.style.color = "green";
+}
+);
+
+
+
+
